5c4f460aba4f0c803c3eeb50785df8ec0f1d2226
陶辰 <chen.tao@ifchange.com>
2020-04-25 22:27:11 +0800

    update

--- mod_learn4/src/read_func.rs
+++ mod_learn4/src/read_func.rs
@@ -1,3 +1,15 @@
+#[cfg(test)]
+mod tests {
+    fn read_func_test() -> String {
+        return "b29827ecc0ced7dc011014868259a780".to_string()
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(read_func_test(), "b29827ecc0ced7dc011014868259a780".to_string());
+    }
+}
+
 mod static_kv;
 
 pub fn read_func() {
